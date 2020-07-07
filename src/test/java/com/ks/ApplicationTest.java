5096cbb68cadf825d3e618f9402b3e90fac5ab29
jmedina <jmedina@kssoluciones.com>
2020-07-07 10:48:41 -0500

    1.0.0.0-SNAPSHOT - TestFixing

--- src/test/java/com/ks/ApplicationTest.java
+++ src/test/java/com/ks/ApplicationTest.java
@@ -16,10 +16,10 @@ class ApplicationTest
         assertEquals(4, Application.duplicateCount("AcCoMmOdAtIOn"));
         assertEquals("h", Application.getMiddle("the"));
         assertEquals("as", Application.getMiddle("easy"));
-        assertEquals(4, Application.persistence(999));
+        assertEquals(2, Application.persistence(999));
         assertEquals(5, Application.getCount("abracadabra"));
         assertEquals(6, Application.digital_root(789));
-        assertArrayEquals(new String[]{"Gi", "ve", "m", "e ", "so", "me", "p", "ai", "rs", "p", "le", "as", "e_"}, Application.solution("Give me some pairs please"));
+        assertArrayEquals(new String[]{"Gi", "ve", " m", "e ", "so", "me", " p", "ai", "rs", " p", "le", "as", "e_"}, Application.solution("Give me some pairs please"));
         assertEquals(2, Application.findShort("Can you think of something more likely"));
         assertEquals("128.15.26.23", Application.longToIP(2148473367L));
         assertEquals(new Integer(5), Application.repeated(new int[]{1, 2, 3, 5, 5, 6, 7, 8, 9, 0, 1}));
